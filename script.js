58276a08cf10b9e83c13f5e76934a16951fb3333
Nataliya Krylova <nataliya.krylova2022@gmail.com>
2023-11-29 15:44:35 +0100

    add multiple color schemes

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ const colorInput = document.getElementById("color-input");
 const mainColorValue = document.querySelector(".main-color-value");
 const mainColorName = document.querySelector(".main-color-name");
 const colorselect = document.getElementById("color-select");
-const colorsBlock = document.getElementById("colors-block");
+const mainElement = document.querySelector("main");
+const colorSchemesBlock = document.querySelector(".color-schemes");
+const colorSchemeTitleInput = document.getElementById("color-scheme-title");
+const schemesTitle = document.querySelector(".schemes-title");
+const colorSchemesObj = [];
 
 colorForm.addEventListener("submit", function (event) {
   event.preventDefault();
-  mainColorValue.textContent = colorInput.value.toUpperCase();
-  colorsBlock.innerHTML = "";
   const color = colorInput.value.slice(1);
   const colorsMode = colorselect.value;
 
@@ -19,7 +21,29 @@ colorForm.addEventListener("submit", function (event) {
         .then((response) => response.json())
         .then((data) => {
           mainColorName.textContent = data.name.value;
+          mainColorValue.textContent = data.hex.value;
 
+          // generate first item with choosen color
+          const colorsBlock = document.createElement("div");
+          colorsBlock.classList.add("colors-block");
+          colorsBlock.innerHTML =
+            `
+                      <h2 class="colors-block-title">${colorSchemeTitleInput.value}</h2>
+                      <button class="delete-scheme-btn" id="delete-scheme-btn"></button>
+                      ` +
+            generateColorItemHTML(data.hex.value, data.name.value);
+
+          /* <div class="color-item">
+                        <div class="color" style="background: ${data.hex.value}"></div>
+                        <h3 class="color-title color-data">${data.name.value}</h3>
+                        <div class="color-subtitle color-data">${data.hex.value}</div>
+                      </div> */
+          colorSchemesBlock.prepend(colorsBlock);
+
+          schemesTitle.textContent =
+            colorSchemesBlock.childNodes.length === 1
+              ? "Your color scheme"
+              : "Your color schemes";
           fetch(
             `https://www.thecolorapi.com/scheme?hex=${color}&format=json&mode=${colorsMode}&count=5`
           )
@@ -29,14 +53,19 @@ colorForm.addEventListener("submit", function (event) {
                 .then((response) => response.json())
                 .then((data) => {
                   let complementColorsHTML = "";
-                  const complementColors = data.colors.forEach((color) => {
-                    complementColorsHTML += `
+                  // generate color scheme
+                  data.colors.forEach((color) => {
+                    complementColorsHTML += generateColorItemHTML(
+                      color.hex.value,
+                      color.name.value
+                    );
+                    /* `
                       <div class="color-item">
                         <div class="color" style="background: ${color.hex.value}"></div>
                         <h3 class="color-title color-data">${color.name.value}</h3>
                         <div class="color-subtitle color-data">${color.hex.value}</div>
                       </div>
-                    `;
+                    `; */
                   });
                   colorsBlock.innerHTML += complementColorsHTML;
                 });
@@ -46,9 +75,41 @@ colorForm.addEventListener("submit", function (event) {
 });
 
 window.addEventListener("click", function (e) {
+  // copy text from color name and value
   if (e.target.classList.contains("color-data")) {
     navigator.clipboard.writeText(e.target.textContent);
-  } else {
-    return;
+  }
+  // delete color scheme
+  else if (e.target.classList.contains("delete-scheme-btn")) {
+    const deleteSchemeBtns = document.querySelectorAll(".delete-scheme-btn");
+    const colorBlocks = this.document.querySelectorAll(".colors-block");
+    let index;
+    console.log(deleteSchemeBtns);
+    deleteSchemeBtns.forEach((btn, i) => {
+      if (e.target === btn) index = i;
+    });
+    colorBlocks[index].remove();
+    console.log(colorBlocks.length);
+    if (colorBlocks.length === 1) {
+      mainColorName.textContent = "";
+      mainColorValue.textContent = "";
+      colorInput.value = "#000000";
+    }
+    schemesTitle.textContent =
+      colorSchemesBlock.childNodes.length === 0
+        ? "You don't have any color scheme yet..."
+        : colorSchemesBlock.childNodes.length === 1
+        ? "Your color scheme"
+        : "Your color schemes";
   }
 });
+
+function generateColorItemHTML(value, name) {
+  return `
+                      <div class="color-item">
+                        <div class="color" style="background: ${value}"></div>
+                        <h3 class="color-title color-data">${name}</h3>
+                        <div class="color-subtitle color-data">${value}</div>
+                      </div>
+                    `;
+}
